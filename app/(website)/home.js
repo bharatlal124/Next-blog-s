c8777f80d30c81aa68e1aec45702c53ffd51b744
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-08-27 18:26:13 +0530

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -15,7 +15,7 @@ export default function Home() {
         const res = await fetch("https://bharat-blog.onrender.com/api/blogs/all");
         const data = await res.json();
         const allPosts = Array.isArray(data) ? data : data.data || [];
-        setPosts(allPosts.slice(0, 4));
+        setPosts(allPosts.slice(0, 8));
       } catch (error) {
         console.error("Failed to fetch posts:", error);
       } finally {
@@ -45,7 +45,7 @@ export default function Home() {
             ))}
           </div>
           <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
-            {posts.slice(0, 8).map(post => (
+            {posts.slice(0, 6).map(post => (
               <PostList key={post._id} post={post} aspect="square" />
             ))}
           </div>
