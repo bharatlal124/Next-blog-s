94a3756dc32ee9a1340b1f4187255b4e90e7cdcf
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-09-01 17:18:22 +0530

    Update about.js

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -13,28 +13,18 @@ export default function About({ authors, settings }) {
         <p className="text-lg">We are a small passionate team.</p>
       </div>
 
-      <div className="mb-16 mt-6 grid grid-cols-3 gap-5 md:mb-32 md:mt-16 md:gap-16">
-        {authors.slice(0, 3).map(author => {
-          const imageProps = urlForImage(author?.image) || null;
-          return (
-            <div
-              key={author._id}
-              className="relative aspect-square overflow-hidden rounded-md bg-slate-50 odd:translate-y-10 odd:md:translate-y-16">
-              <Link href={`/author/${author?.slug}`}>
-                {imageProps && (
-                  <Image
-                    src={imageProps?.src}
-                    alt={author?.name || " "}
-                    fill
-                    sizes="(max-width: 320px) 100vw, 320px"
-                    className="object-cover"
-                  />
-                )}
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+       <div className="flex items-center justify-center h-screen">
+  <div className="w-72 h-72 rounded-full overflow-hidden">
+    <Image
+      src="/about-img2.jpg"  
+      alt="Sample Image"
+      width={288}   
+      height={288}  
+      className="object-cover w-full h-full"
+    />
+  </div>
+</div>
+
 
       <div className="prose mx-auto mt-14 text-center dark:prose-invert">
         <p>
